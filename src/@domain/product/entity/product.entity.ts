82027879be4890bd7c7b69c8c671f0587180be7a
JulioOLV <julioccolv@gmail.com>
2024-02-14 20:37:55 -0300

    feat: adds product entity

---
+++ src/@domain/product/entity/product.entity.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from 'uuid';
+
+export default class Product {
+  private readonly _id: string;
+  private readonly _name: string;
+  private _active: boolean;
+
+  constructor(id: string, name: string) {
+    this._id = id || uuid();
+    this._name = name;
+
+    this.validate();
+  }
+
+  public get active(): boolean {
+    return this._active;
+  }
+
+  public activeProduct(active: boolean): void {
+    this._active = active;
+  }
+
+  private validate(): void {
+    if (!this._name) {
+      throw new Error('Name is required');
+    }
+  }
+}
